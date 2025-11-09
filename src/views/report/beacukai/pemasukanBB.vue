b787fc4442406808e453bd949b938069d883d535
ariefwl <ariefwl79@gmail.com>
2025-11-09 13:20:05 +0700

    perbaikan index.js

--- src/views/report/beacukai/pemasukanBB.vue
+++ src/views/report/beacukai/pemasukanBB.vue
@@ -14,56 +14,42 @@
                     <CTable>
                         <CTableHead>
                             <CTableRow>
-                            <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-                            <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-                            <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Tgl. Rekam</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Jenis Dokumen</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">No. PIB</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Tgl. PIB</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">No. Seri Barang</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">No. BTB</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Kode Barang</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">HS. Code</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Nama Barang</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Satuan</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">QTY</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Mata Uang</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Nilai Barang</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Sub Kontrak</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Negara Asal</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Harga Satuan</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Rate</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Harga Satuan Konversi</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Nilai Barang Konversi</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">No. PO</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Fasilitas</CTableHeaderCell>
+                            <CTableHeaderCell scope="col">Inv. No</CTableHeaderCell>
                             </CTableRow>
                         </CTableHead>
                         <CTableBody>
-                            <CTableRow>
-                            <CTableHeaderCell scope="row">Default</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="primary">
-                            <CTableHeaderCell scope="row">Primary</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="secondary">
-                            <CTableHeaderCell scope="row">Secondary</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="success">
-                            <CTableHeaderCell scope="row">Success</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="danger">
-                            <CTableHeaderCell scope="row">Danger</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="warning">
-                            <CTableHeaderCell scope="row">Warning</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="info">
-                            <CTableHeaderCell scope="row">Info</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="light">
-                            <CTableHeaderCell scope="row">Light</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            </CTableRow>
-                            <CTableRow color="dark">
-                            <CTableHeaderCell scope="row">Dark</CTableHeaderCell>
-                            <CTableDataCell>Cell</CTableDataCell>
-                            <CTableDataCell>Cell</CTableDataCell>
+                            <CTableRow v-for="(item, index) in purchase" :key="index">
+                                <CTableDataCell>{{ item.GRDate }}</CTableDataCell>
+                                <CTableDataCell>{{ item.DocType }}</CTableDataCell>
+                                <CTableDataCell>{{ item.PIBNo }}</CTableDataCell>
+                                <CTableDataCell>{{ item.PIBDate }}</CTableDataCell>
+                                <CTableDataCell>{{ item.IDNo }}</CTableDataCell>
+                                <CTableDataCell>{{ item.GRNo }}</CTableDataCell>
+                                <CTableDataCell>{{ item.ItemCode }}</CTableDataCell>
+                                <CTableDataCell>{{ item.ItemName }}</CTableDataCell>
+                                <CTableDataCell>{{ item.Qty }}</CTableDataCell>
+                                <CTableDataCell>{{ item.Period }}</CTableDataCell>
                             </CTableRow>
                         </CTableBody>
                         </CTable>
@@ -72,3 +58,24 @@
         </CCol>
     </CRow>
 </template>
+
+<script setup>
+    import { ref, onMounted } from 'vue'
+    import axios from 'axios'
+
+    const purchase = ref([])
+    const loading = ref(true)
+    const error = ref(null)
+    
+    onMounted(async () => {
+        try {
+            const response = await axios.get(`http://103.255.240.205:3000/api/purchase?period=202401`)
+            purchase.value = response.data
+        } catch (err) {
+            console.error(err)
+            error.value = 'Gagal memuat data dari server !'
+        } finally {
+            loading.value = false
+        }
+    })
+</script>
